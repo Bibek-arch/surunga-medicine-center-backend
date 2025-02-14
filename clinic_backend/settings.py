3fc059d4b373f2856406c53c5f9c37deb23fb2ef
Bibek-arch <bbeksubed@gmail.com>
2025-02-14 20:52:38 +0545

    pushdbpostgre

--- clinic_backend/settings.py
+++ clinic_backend/settings.py
@@ -111,12 +111,23 @@ WSGI_APPLICATION = 'clinic_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # Supabase default DB name
+        'USER': 'postgres',
+        'PASSWORD': 'surunga123',
+        'HOST': 'db.wkgquanhuhbrllticpbv.supabase.co',  # Supabase host
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
